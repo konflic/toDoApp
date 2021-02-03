053784460ecb998bdf09dad65d5b31ac7a74800b
Mikhail Chirkov <mchirkov@ivi.ru>
2021-02-03 15:54:26 +0300

    Room database in progress

---
+++ app/src/main/java/com/example/todoapp/data/models/ToDoData.kt
@@ -0,0 +1,15 @@
+package com.example.todoapp.data.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.todoapp.data.models.Priority
+
+
+@Entity(tableName = "todo_table")
+data class ToDoData(
+        @PrimaryKey(autoGenerate = true)
+        val id: Int,
+        val title: String,
+        val priority: Priority,
+        val description: String
+)
